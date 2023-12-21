de1ae41c2992dd345b153295c2c70e24305b8d9e
itsmahi2915 <121787063+itsmahi2915@users.noreply.github.com>
2023-12-21 04:20:00 +0000

    tip calculator
    Also added README

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ total_tip_amount = bill * tip_as_percent
 total_bill = bill + total_tip_amount
 bill_per_person = total_bill / people
 final_amount = round(bill_per_person,2)
+final_amount = "{:.2f}".format(final_amount)
 print(f"Each person should pay: rs{final_amount}")
 
             
